e5c6a6d578be4df745325a63ff980ace36482047
Sarnendu Das <87897770+sarnendu1@users.noreply.github.com>
2022-01-20 01:27:39 +0530

    updated

---
+++ server/routes/route.js
@@ -0,0 +1,20 @@
+import express from  'express';
+import { getProductById, getProducts } from '../controller/product-controller.js';
+import { userSignUp, userLogIn } from '../controller/user-controller.js';
+import { addItemInCart } from '../controller/cart-controller.js';
+
+
+const router = express.Router();
+
+//login & signup
+router.post('/signup', userSignUp);
+router.post('/login', userLogIn);
+
+router.get('/products', getProducts);
+router.get('/product/:id', getProductById);
+
+router.post('/cart/add', addItemInCart);
+
+
+
+export default router;
